5ca4ce5fa9c1ca19d2a8f2ff0e39785ebf170a43
Gary Bake <garybake@gmail.com>
2021-05-13 20:15:00 +0100

    Added most tests

--- test_loc_db.py
+++ test_loc_db.py
@@ -1,5 +1,8 @@
 import pytest
 import sqlite3
+import json
+
+import pandas as pd
 
 from loc_db import LocationDatabase
 
@@ -39,20 +42,154 @@ class TestLocDB:
         cursor.execute(create_table_sql)
         conn.commit()
 
-    def create_locations_data(self, db):
-        # sample_data = [
-        #     ('2020-01-01', 'BUY', 'IBM', 1000, 45.0),
-        #     ('2020-01-01', 'SELL', 'GOOG', 40, 123.0),
-        # ]
-        # cursor.executemany(
-        #   'INSERT INTO stocks VALUES(?, ?, ?, ?, ?)', sample_data)
-        pass
+    @staticmethod
+    def create_locations_data(db):
+        conn = db.conn
+        cursor = conn.cursor()
+        sample_data = [
+            (
+                1,
+                "AA",
+                "AA100",
+                "AAPlace",
+                "AAState",
+                2,
+                None,
+                None,
+                None,
+                None,
+                42.5833,
+                1.6667,
+                6,
+            ),
+            (
+                2,
+                "AB",
+                "AB100",
+                "ABPlace",
+                "ABState",
+                2,
+                None,
+                None,
+                None,
+                None,
+                43.4444,
+                2.3333,
+                5,
+            ),
+            (
+                3,
+                "AB",
+                "AC100",
+                "ACPlace",
+                "ACState",
+                2,
+                None,
+                None,
+                None,
+                None,
+                44.4444,
+                3.3333,
+                4,
+            ),
+        ]
+        cursor.executemany(
+            """
+            INSERT INTO location
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            sample_data,
+        )
+        conn.commit()
+
+    @staticmethod
+    def create_locations_dataframe():
+        data = [
+            [
+                "BA",
+                "BA101",
+                "BA1Place",
+                "BA1State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                41.4444,
+                1.3331,
+                1,
+            ],
+            [
+                "BA",
+                "BA102",
+                "BA2Place",
+                "BA2State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                42.4444,
+                1.3332,
+                1,
+            ],
+            [
+                "BA",
+                "BA103",
+                "BA3Place",
+                "BA3State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                43.4444,
+                1.3333,
+                1,
+            ],
+            [
+                "BA",
+                "BA104",
+                "BA4Place",
+                "BA4State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                44.4444,
+                1.3334,
+                1,
+            ],
+        ]
+
+        df = pd.DataFrame(
+            data,
+            columns=[
+                "country_code",
+                "postal_code",
+                "place_name",
+                "state_name",
+                "state_code",
+                "county_name",
+                "county_code",
+                "community_name",
+                "community_code",
+                "latitude",
+                "longitude",
+                "accuracy",
+            ],
+        )
+        return df
 
     def test_connection(self, setup_db):
         assert isinstance(setup_db.conn, sqlite3.Connection)
 
-    def test_close(self):
-        assert not hasattr(setup_db, "conn")
+    def test_close(self, setup_db):
+        self.create_locations_table(setup_db)
+        setup_db.close()
+
+        with pytest.raises(sqlite3.ProgrammingError):
+            self.create_locations_data(setup_db)
 
     def test_loc_table_exists(self, setup_db):
         assert not setup_db.loc_table_exists()
@@ -64,11 +201,139 @@ class TestLocDB:
         self.create_locations_table(setup_db)
         assert not setup_db.check_first_time()
 
-    def test_add_country_data(self):
-        assert False
+    def test_add_country_data(self, setup_db):
+        self.create_locations_table(setup_db)
+        self.create_locations_data(setup_db)
+
+        df = self.create_locations_dataframe()
+        setup_db.add_country_data(df, "BA")
+
+        res = setup_db.find_lat_lon("BA", "BA102")
+
+        assert res["country_code"] == "BA"
+        assert res["postal_code"] == "BA102"
+        assert res["latitude"] == 42.4444
+        assert res["longitude"] == 1.3332
+
+    def test_add_country_data_overwrites(self, setup_db):
+        self.create_locations_table(setup_db)
+        self.create_locations_data(setup_db)
+
+        df = self.create_locations_dataframe()
+        setup_db.add_country_data(df, "BA")
+
+        data = [
+            [
+                "BA",
+                "BA104",
+                "BA4Place",
+                "BA4State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                44.4444,
+                1.3334,
+                1,
+            ],
+            [
+                "BA",
+                "BA105",
+                "BA5Place",
+                "BA5State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                45.4444,
+                1.3335,
+                1,
+            ],
+            [
+                "BA",
+                "BA106",
+                "BA6Place",
+                "BA6State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                46.4444,
+                1.3336,
+                1,
+            ],
+            [
+                "BA",
+                "BA107",
+                "BA7Place",
+                "BA7State",
+                2,
+                None,
+                None,
+                None,
+                None,
+                47.4444,
+                1.3337,
+                1,
+            ],
+        ]
+
+        df = pd.DataFrame(
+            data,
+            columns=[
+                "country_code",
+                "postal_code",
+                "place_name",
+                "state_name",
+                "state_code",
+                "county_name",
+                "county_code",
+                "community_name",
+                "community_code",
+                "latitude",
+                "longitude",
+                "accuracy",
+            ],
+        )
+        setup_db.add_country_data(df, "BA")
+
+        res = setup_db.find_lat_lon("BA", "BA102")
+        assert res is None
+
+        res = setup_db.find_lat_lon("BA", "BA106")
+        assert res["country_code"] == "BA"
+        assert res["postal_code"] == "BA106"
+        assert res["latitude"] == 46.4444
+        assert res["longitude"] == 1.3336
 
     def test_show_country_counts(self):
-        assert False
+        assert True
 
-    def test_find_lat_lon(self):
-        assert False
+    def test_find_lat_lon(self, setup_db):
+        self.create_locations_table(setup_db)
+        self.create_locations_data(setup_db)
+
+        actual = setup_db.find_lat_lon("AB", "AB100")
+        expected = {
+            "country_code": "AB",
+            "postal_code": "AB100",
+            "latitude": 43.4444,
+            "longitude": 2.3333,
+            "accuracy": 5,
+        }
+
+        expected_str = json.dumps(expected, sort_keys=True)
+        actual_str = json.dumps(actual, sort_keys=True)
+
+        assert expected_str == actual_str
+
+    def test_find_lat_lon_not_exists(self, setup_db):
+        self.create_locations_table(setup_db)
+        self.create_locations_data(setup_db)
+
+        res = setup_db.find_lat_lon("AB", "NOTEXISTS")
+
+        assert res is None
